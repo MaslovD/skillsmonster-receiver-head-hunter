7c81d3556282aa12f9b0100be643ccc9b564bcd5
dmaslov <masdmtr@gmail.com>
2017-07-09 03:10:57 +0300

    Initial commit

---
+++ src/main/java/com/masdmtr/skillsmonster/SkillsMonsterApplication.java
@@ -0,0 +1,12 @@
+package com.masdmtr.skillsmonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsMonsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsMonsterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/masdmtr/skillsmonster/SkillsMonsterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.masdmtr.skillsmonster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkillsMonsterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
